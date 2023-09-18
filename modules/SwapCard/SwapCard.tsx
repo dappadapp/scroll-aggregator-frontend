5edec6bf101ca6127b58ae52405cc92257e851f9
enesbugrac <enesbc46@gmail.com>
2023-09-18 11:47:37 +0300

    feat:lifi widget

--- modules/SwapCard/SwapCard.tsx
+++ modules/SwapCard/SwapCard.tsx
@@ -13,28 +13,29 @@ import addresses from "@/constants/contracts";
 import { ChainId, Currency } from "@/types";
 import SwapModal from "./SwapModal";
 
-import SyncSwapPoolFactoryAbi from "@/constants/abis/basePoolFactory.json"
-import SyncSwapClassicPool from "@/constants/abis/SyncSwapClassicPool.json"
-import SyncSwapStablePool from "@/constants/abis/SyncSwapStablePool.json"
-import IconSlider from '@/assets/images/icon-sliders.svg'
-import IconRefresh from '@/assets/images/icon-refresh.svg'
+import SyncSwapPoolFactoryAbi from "@/constants/abis/basePoolFactory.json";
+import SyncSwapClassicPool from "@/constants/abis/SyncSwapClassicPool.json";
+import SyncSwapStablePool from "@/constants/abis/SyncSwapStablePool.json";
+import IconSlider from "@/assets/images/icon-sliders.svg";
+import IconRefresh from "@/assets/images/icon-refresh.svg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowsUpDown } from "@fortawesome/free-solid-svg-icons";
 
-type Props = {
-};
+type Props = {};
 
 const percentageButtons = [25, 50, 75, 100];
 
 const SwapCard: React.FC<Props> = () => {
   const { open } = useWeb3Modal();
-  const { address, isConnected } = useAccount();  
+  const { address, isConnected } = useAccount();
   const [swapAmount, setSwapAmount] = useState(0);
   const [receiveAmount, setReceiveAmount] = useState(0);
   const [isSwapModalOpen, setIsSwapModalOpen] = useState(false);
   //TODO: Add tokens
   const [tokenFrom, setTokenFrom] = useState<Currency>();
-  const [tokenTo, setTokenTo] = useState<Currency | undefined>(Tokens[ChainId.SCROLL_TESTNET].mock);
+  const [tokenTo, setTokenTo] = useState<Currency | undefined>(
+    Tokens[ChainId.SCROLL_TESTNET].mock
+  );
   const [isChangeFrom, setChangeFrom] = useState(true);
 
   const { data: balanceFrom, isLoading: isLoadingBalanceFrom } = useBalance({
@@ -43,8 +44,8 @@ const SwapCard: React.FC<Props> = () => {
       token: tokenFrom?.wrapped.address,
     }),
     chainId: tokenFrom?.chainId,
-    enabled: !!tokenFrom,    
-  })
+    enabled: !!tokenFrom,
+  });
 
   const { data: balanceTo, isLoading: isLoadingBalanceTo } = useBalance({
     address: address,
@@ -52,75 +53,83 @@ const SwapCard: React.FC<Props> = () => {
       token: tokenTo?.wrapped.address,
     }),
     chainId: tokenTo?.chainId,
-    enabled: !!tokenTo,    
-  })
+    enabled: !!tokenTo,
+  });
 
   const { data: poolAddress } = useContractRead({
     address: addresses.syncswapClassicPoolFactory,
     abi: SyncSwapPoolFactoryAbi,
     functionName: "getPool",
     args: [tokenFrom?.wrapped.address, tokenTo?.wrapped.address],
-    enabled: !!tokenFrom && !!tokenTo
+    enabled: !!tokenFrom && !!tokenTo,
   });
 
   const { data: outAmount } = useContractRead({
     address: poolAddress as `0x${string}`,
     abi: SyncSwapClassicPool,
     functionName: "getAmountOut",
-    args: [tokenFrom?.wrapped.address, parseUnits(`${swapAmount.toFixed(10)}`, tokenFrom?.decimals || 18), address],
-    enabled: !!poolAddress && !!tokenFrom && isChangeFrom
+    args: [
+      tokenFrom?.wrapped.address,
+      parseUnits(`${swapAmount.toFixed(10)}`, tokenFrom?.decimals || 18),
+      address,
+    ],
+    enabled: !!poolAddress && !!tokenFrom && isChangeFrom,
   });
 
   const { data: inAmount } = useContractRead({
     address: poolAddress as `0x${string}`,
     abi: SyncSwapClassicPool,
     functionName: "getAmountIn",
-    args: [tokenTo?.wrapped.address, parseUnits(`${receiveAmount.toFixed(10)}`, tokenTo?.decimals || 18), address],
-    enabled: !!poolAddress && !!tokenTo && !isChangeFrom
+    args: [
+      tokenTo?.wrapped.address,
+      parseUnits(`${receiveAmount.toFixed(10)}`, tokenTo?.decimals || 18),
+      address,
+    ],
+    enabled: !!poolAddress && !!tokenTo && !isChangeFrom,
   });
 
-
   useEffect(() => {
-    if( outAmount !== undefined && tokenTo && isChangeFrom ) {
-      setReceiveAmount(+(+formatUnits(outAmount as bigint, tokenTo.decimals)).toFixed(10))
+    if (outAmount !== undefined && tokenTo && isChangeFrom) {
+      setReceiveAmount(
+        +(+formatUnits(outAmount as bigint, tokenTo.decimals)).toFixed(10)
+      );
     }
-  }, [outAmount, tokenTo, isChangeFrom])
+  }, [outAmount, tokenTo, isChangeFrom]);
 
   useEffect(() => {
-    if( inAmount !== undefined && tokenFrom && !isChangeFrom  ) {
-      setSwapAmount(+formatUnits(inAmount as bigint, tokenFrom.decimals))
+    if (inAmount !== undefined && tokenFrom && !isChangeFrom) {
+      setSwapAmount(+formatUnits(inAmount as bigint, tokenFrom.decimals));
     }
-  }, [inAmount, tokenFrom, !isChangeFrom])
+  }, [inAmount, tokenFrom, !isChangeFrom]);
 
-  const native = useNativeCurrency()
+  const native = useNativeCurrency();
 
   useEffect(() => {
-    setTokenFrom(native.wrapped)
-  }, [native])
-  
+    setTokenFrom(native.wrapped);
+  }, [native]);
+
   const handleSwitchToken = () => {
     setTokenFrom(tokenTo);
     setTokenTo(tokenFrom);
-  }
+  };
 
   const handleClickInputPercent = (percent: number) => {
-    if( !balanceFrom || !tokenFrom ) 
-      return;
-    const balance = formatUnits(balanceFrom.value, tokenFrom?.decimals) 
-    setSwapAmount(parseInt(balance) * percent / 100);
+    if (!balanceFrom || !tokenFrom) return;
+    const balance = formatUnits(balanceFrom.value, tokenFrom?.decimals);
+    setSwapAmount((parseInt(balance) * percent) / 100);
     setChangeFrom(true);
-  }
+  };
 
   const onKeyDownSwapAmount = () => {
     setChangeFrom(true);
-  }
+  };
 
   const onKeyDownReceiveAmount = () => {
     setChangeFrom(false);
-  }
+  };
 
   return (
-    <div className="w-full max-w-[548px] relative p-8 gap-2 flex flex-col relative border-r border-white/10 bg-white/5 rounded-l-2xl mx-auto my-4">
+    <div className="w-full max-w-[548px] p-8 gap-2 flex flex-col relative border-r border-white/10 bg-white/5 rounded-l-2xl mx-auto my-4">
       <div className={`w-full h-full gap-4 flex-1 flex justify-between flex-col`}>
         <div className="flex items-center gap-2">
           <h1 className="font-semibold text-3xl">SWAP</h1>
@@ -144,20 +153,21 @@ const SwapCard: React.FC<Props> = () => {
                   placeholder="Enter Amount"
                   className="w-full crosschainswap-input"
                 />
-                {balanceFrom &&
+                {balanceFrom && (
                   <div className="mt-2">
                     Balance: {balanceFrom.formatted} {balanceFrom.symbol}
                   </div>
-                }
+                )}
               </div>
-              <TokenSelect
-                onChange={setTokenFrom}
-                token={tokenFrom}
-              />
+              <TokenSelect onChange={setTokenFrom} token={tokenFrom} />
             </div>
             <div className="grid grid-cols-2 md:grid-cols-4 gap-2">
               {percentageButtons.map((val, index) => (
-                <Button className="font-inter text-sm" key={"perc-button-" + index} onClick={() => handleClickInputPercent(val)}>
+                <Button
+                  className="font-inter text-sm"
+                  key={"perc-button-" + index}
+                  onClick={() => handleClickInputPercent(val)}
+                >
                   {val}%
                 </Button>
               ))}
@@ -181,22 +191,24 @@ const SwapCard: React.FC<Props> = () => {
                   placeholder="Receive Amount"
                   className="crosschainswap-input w-full"
                 />
-                {balanceTo &&
+                {balanceTo && (
                   <div className="mt-2">
                     Balance: {balanceTo.formatted} {balanceTo.symbol}
                   </div>
-                }
+                )}
               </div>
-              <TokenSelect
-                onChange={setTokenTo}
-                token={tokenTo}
-              />
+              <TokenSelect onChange={setTokenTo} token={tokenTo} />
             </div>
           </div>
         </div>
-        
-        <Button variant="bordered" disabled={isConnected && (!tokenFrom || !tokenTo || !swapAmount)} className="w-full p-4 rounded-lg text-xl font-semibold" onClick={() => isConnected ? setIsSwapModalOpen(true) : open()}>
-          {isConnected ? 'SWAP' : 'CONNECT WALLET'}
+
+        <Button
+          variant="bordered"
+          disabled={isConnected && (!tokenFrom || !tokenTo || !swapAmount)}
+          className="w-full p-4 rounded-lg text-xl font-semibold"
+          onClick={() => (isConnected ? setIsSwapModalOpen(true) : open())}
+        >
+          {isConnected ? "SWAP" : "CONNECT WALLET"}
         </Button>
       </div>
       {tokenFrom && tokenTo && isSwapModalOpen && poolAddress ? (
