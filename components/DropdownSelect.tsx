7af7a22a78f7c6d82c713f1a17e1fe6ef6f0e2d9
enesbugrac <enesbc46@gmail.com>
2023-09-21 19:12:36 +0300

    style: changes

--- components/DropdownSelect.tsx
+++ components/DropdownSelect.tsx
@@ -17,19 +17,19 @@ type Props = {
 
 const DropdownSelect = ({
   value,
-  className = '',
-  dropdownClassName = '',
+  className = "",
+  dropdownClassName = "",
   onChange,
   options,
   optionRenderer,
   onSearch,
-  children
+  children,
 }: Props) => {
   return (
     <Listbox value={value} onChange={onChange}>
       <div className="relative">
         <Listbox.Button
-          className={`${className} flex items-center justify-between relative w-full cursor-pointer rounded-lg bg-white/5 gap-2 p-2 break-words overflow-hidden text-left text-lg focus:outline-none`}
+          className={`${className} flex items-center justify-between relative w-full cursor-pointer rounded-lg bg-white/5 gap-2 p-2 break-words overflow-hidden text-left text-xs md:text-lg focus:outline-none`}
         >
           {children}
           <span className="pointer-events-none inset-y-0 flex items-center">
@@ -42,15 +42,17 @@ const DropdownSelect = ({
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
-          <Listbox.Options className={`${dropdownClassName} absolute right-0 mt-1 z-20 flex flex-col items-center max-h-60 overflow-auto rounded-md bg-[#202020] backdrop-blur-xl py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm`}>
-            {onSearch && 
-            <input
-              type="text"
-              onChange={(e) => onSearch(e.target.value)}
-              className="my-1 w-5/6 px-1 py-1 text-xs md:text-base rounded-md bg-gray-700"
-              placeholder="Search"
-            />
-            }
+          <Listbox.Options
+            className={`${dropdownClassName} absolute right-0 mt-1 z-20 flex flex-col items-center max-h-60 overflow-auto rounded-md bg-[#202020] backdrop-blur-xl py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm`}
+          >
+            {onSearch && (
+              <input
+                type="text"
+                onChange={(e) => onSearch(e.target.value)}
+                className="my-1 w-5/6 px-1 py-1 text-xs md:text-base rounded-md bg-gray-700"
+                placeholder="Search"
+              />
+            )}
             {options.map((option, i) => (
               <Listbox.Option
                 key={i}
