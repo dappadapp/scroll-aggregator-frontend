82fc47b4b48e41a3bd36dabeac3901766ccdde61
Emre Ulgac <ulgacemre@gmail.com>
2023-09-27 14:15:47 +0000

    Currency logo size fix

--- components/CurrencyLogo.tsx
+++ components/CurrencyLogo.tsx
@@ -16,7 +16,7 @@ export function CurrencyLogo({
       src={currency?.logo}
       alt={`${currency?.symbol ?? "token"} logo`}
       style={style}
-      className={`rounded-full w-4 h-4 lg:w-${size} lg:h-${size}`}
+      className={`rounded-full w-10 h-${size} lg:w-${size} lg:h-${size}`}
     />
   ) : (
     <div className={`bg-white/10 rounded-full w-${size} h-${size}`} />
--- components/SwapToken.tsx
+++ components/SwapToken.tsx
@@ -12,7 +12,7 @@ export const SwapToken: React.FC<Props> = (props) => {
     <div
       className="flex items-center gap-2"
     >
-      <CurrencyLogo currency={props.currency} size={10} />
+      <CurrencyLogo currency={props.currency} size={8} />
       <div className={`flex flex-col text-left`}>
         <span className="text-sm md:text-xl">{props.value}</span>
         <span className="text-[#AAA] text-xs md:text-sm">{props.currency.symbol}</span>
