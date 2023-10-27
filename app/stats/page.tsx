2eb68913421cdd14687953f4250a388076585bdc
enesbugrac <enesbc46@gmail.com>
2023-10-28 00:01:07 +0300

    feat: responsive and date

--- app/stats/page.tsx
+++ app/stats/page.tsx
@@ -22,7 +22,7 @@ function Page() {
     const response = await axios.get("/api/stats");
     setStatsData(response.data);
   };
-  const timestampToDate = (timestamp: string) => {
+  const timestampToDate = (timestamp: number) => {
     let today = new Date(timestamp);
 
     let date =
@@ -39,8 +39,8 @@ function Page() {
     </div>
   ) : (
     <div className="flex w-full flex-col gap-12 text-[#FFF0DD] justify-start lg:mt-12 mb-10 mt-10">
-        <h1 className="text-lg lg:text-[64px] font-bold">Statistics</h1>
-    {/*  
+      <h1 className="text-lg lg:text-[64px] font-bold">Statistics</h1>
+      {/*  
       <span className="lg:text-base font-normal">
       The stats page of a DEX aggregator 
       The stats page of the <span className="text-[#FF7C5C]">Aggre</span>, provides users with a comprehensive overview of the platform's performance and key metrics.{" "}
@@ -50,8 +50,8 @@ function Page() {
         <span className="text-[#FF7C5C]"> decentralized exchange ecosystem.</span>
       </span>
 
-      */} 
-      <div className="flex flex-col lg:flex-row justify-center gap-4">
+      */}
+      <div className="flex flex-col min-[1300px]:flex-row justify-center gap-4">
         <VolumeCart
           volume_day={Number(Number(statsData?.volume24h).toFixed(2))}
           volume_total={Number(Number(statsData?.totalVolume).toFixed(2))}
@@ -65,10 +65,10 @@ function Page() {
             <td className="overflow-hidden whitespace-nowrap w-[12%] pl-2 py-3">Dex</td>
             <td className="overflow-hidden  whitespace-nowrap w-[12%]  py-3">Address</td>
             <td className="table-cell py-3 w-[12%]">Transaction</td>
-            <td className=" hidden lg:table-cell py-3 w-[12%]">From Amount</td>
-            <td className=" hidden lg:table-cell py-3 w-[12%]">To Amount</td>
-            <td className=" hidden lg:table-cell  py-3 w-[15%]">To Token</td>
-            <td className=" hidden lg:table-cell  py-3 w-[15%]">From Token</td>
+            <td className=" hidden min-[1300px]:table-cell py-3 w-[12%]">From Amount</td>
+            <td className=" hidden min-[1300px]:table-cell py-3 w-[12%]">To Amount</td>
+            <td className=" hidden min-[1300px]:table-cell  py-3 w-[15%]">To Token</td>
+            <td className=" hidden min-[1300px]:table-cell  py-3 w-[15%]">From Token</td>
             <td className=" table-cell text-right py-3 w-[10%] pr-2">Timestamp</td>
           </tr>
           {statsData?.last20tx?.map((item: any, index: number) => {
@@ -91,20 +91,20 @@ function Page() {
                     {formatAddress(item?.tx?.txHash)}
                   </a>
                 </td>
-                <td className="lg:text-base hidden lg:table-cell w-[12%] ">
+                <td className="lg:text-base hidden min-[1300px]:table-cell w-[12%] ">
                   {item?.outUsd.toFixed(2)}$
                 </td>
-                <td className="hidden lg:table-cell lg:text-base w-[15%]">
+                <td className="hidden min-[1300px]:table-cell lg:text-base w-[15%]">
                   {item?.inUsd.toFixed(2)}$
                 </td>
-                <td className="hidden lg:table-cell  w-[15%] lg:text-base">
+                <td className="hidden min-[1300px]:table-cell  w-[15%] lg:text-base">
                   {formatAddress(item?.tx?.toTokenAddress)}
                 </td>
-                <td className="hidden lg:table-cell  w-[15%] lg:text-base">
+                <td className="hidden min-[1300px]:table-cell  w-[15%] lg:text-base">
                   {formatAddress(item?.tx?.fromTokenAddress)}
                 </td>
                 <td className=" text-right w-[10%] lg:text-base pr-2 py-2">
-                  {timestampToDate(item?.tx?.timestamp)}
+                  {timestampToDate(item?.tx?.timestamp * 1000)}
                 </td>
               </tr>
             );
