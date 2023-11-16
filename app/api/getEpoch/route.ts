4074bc6e31545fb04126ddcbadec3144ce45720b
enesbugrac <enesbc46@gmail.com>
2023-11-17 00:28:42 +0300

    feat: epoch date from api

---
+++ app/api/getEpoch/route.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  const headers = {
+    "Content-Type": "application/json",
+    "x-api-key": process.env.NEXT_PUBLIC_NEW_API_KEY,
+  };
+
+  const statusDataResponse = await axios.get(
+    `${process.env.NEXT_PUBLIC_NEW_BASE_URL}/aggre/mainnet/phases`,
+    {
+      headers,
+    }
+  );
+
+  return NextResponse.json(statusDataResponse.data);
+}
